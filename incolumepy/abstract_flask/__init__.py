d20e352e67233062e701dd41cb1f86175dcec154
Britodfbr <britodfbr@gmail.com>
2021-07-19 10:06:12 -0300

    aplicado blueprint webui ao projeto

--- incolumepy/abstract_flask/__init__.py
+++ incolumepy/abstract_flask/__init__.py
@@ -2,11 +2,18 @@
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
 from pathlib import Path
-from flask import Flask, render_template, url_for, send_from_directory
+from flask import Flask, url_for, send_from_directory
+from . import webui
+
+
+def minimal_app():
+    app = Flask(__name__)
+    return app
 
 
 def create_app():
-    app = Flask(__name__)
+    app = minimal_app()
+    webui.init_app(app)
     # app.add_url_rule('/favicon.ico',
     #                  redirect_to=url_for('static', filename='images/favicons/favicon.ico'))
 
