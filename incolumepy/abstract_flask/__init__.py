6e04d649ec420ebd37e7c616db9c7f8331bff17c
Britodfbr <britodfbr@gmail.com>
2021-07-22 14:00:36 -0300

    Tratativa de roteamento para favicon.ico

--- incolumepy/abstract_flask/__init__.py
+++ incolumepy/abstract_flask/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = '@britodfbr'
 from pathlib import Path
-from flask import Flask, url_for, send_from_directory
+from flask import Flask, send_from_directory, send_file, redirect, url_for
 from .ext import webui
 
 
@@ -17,10 +17,22 @@ def create_app():
     # app.add_url_rule('/favicon.ico',
     #                  redirect_to=url_for('static', filename='images/favicons/favicon.ico'))
 
-    # @app.route('/favicon.ico')
-    # def favicon():
-    #     ico = Path(app.root_path)
-    #     return send_from_directory(ico, 'static', 'images/favicons/favicon.ico', mimetype='image/vnd.microsoft.icon')
+    @app.route("/f.ico")
+    def f():
+        return redirect(url_for('static', filename="images/favicons/favicon.ico"))
+
+    @app.route("/fav.ico")
+    def fav():
+        ico = Path(app.root_path) / 'static/images/favicons/favicon.ico'
+        return send_file(ico, mimetype='image/vnd.microsoft.icon')
+
+    @app.route('/favi.ico')
+    def favi():
+        return send_from_directory(app.root_path, path="images/favicons", filename="favicon.ico", mimetype='image/vnd.microsoft.icon')
+
+    @app.route("/favic.ico")
+    def favic():
+        return url_for('static', filename='images/favicons/favicon.ico')
 
     @app.route('/hello')
     def hello():
