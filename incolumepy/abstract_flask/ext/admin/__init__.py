4d9f85b6a33f67f8743765e50a267c35ded9929f
Britodfbr <britodfbr@gmail.com>
2021-07-24 10:39:52 -0300

    Modelos Post, Comment, Category adicionados em AdminZone

--- incolumepy/abstract_flask/ext/admin/__init__.py
+++ incolumepy/abstract_flask/ext/admin/__init__.py
@@ -4,14 +4,20 @@ __author__ = "@britodfbr"
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from incolumepy.abstract_flask.ext.dbase import db
-from incolumepy.abstract_flask.ext.dbase.models import User
+from incolumepy.abstract_flask.ext.dbase.models import User, Post, Comment, Category
 
 admin = Admin()
+
+
+class UserAdmin(ModelView):
+    """Interface administrativa para User"""
 
 
 def init_app(app):
     admin.name = "Abstract Admin"
     admin.template_mode = "bootstrap4"
     admin.init_app(app)
-    admin.add_view(ModelView(User, db.session))
-
+    admin.add_view(UserAdmin(User, db.session))
+    admin.add_view(ModelView(Post, db.session))
+    admin.add_view(ModelView(Comment, db.session))
+    admin.add_view(ModelView(Category, db.session))
