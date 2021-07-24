5132e1ffb8f806864eee3823feef986bccd30c03
Britodfbr <britodfbr@gmail.com>
2021-07-24 11:41:49 -0300

    Personalizado a exibição email e birthdate

--- incolumepy/abstract_flask/ext/admin/__init__.py
+++ incolumepy/abstract_flask/ext/admin/__init__.py
@@ -5,17 +5,24 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from incolumepy.abstract_flask.ext.dbase import db
 from incolumepy.abstract_flask.ext.dbase.models import User, Post, Comment, Category
-
+import re
 admin = Admin()
 
 
 def format_mail(instance, request, user, *args, **kwargs):
-    return user.email.upper()
+    # return f"{user.email[:user.email.index('@')+1]}.."
+    regex = r"(.*@)(.*)(\.\w{3}(\.\w{2})?)"
+    match = re.match(regex, user.email, re.I)
+    return f"{match.group(1)}..{match.group(3)}"
 
 
 class UserAdmin(ModelView):
     """Interface administrativa para User"""
-    column_formatters = {"email": format_mail}
+    column_formatters = {
+        "email": format_mail,
+        "birthdate": lambda i, r, u, *args, **kwargs: f"{u.birthdate:%F}",
+    }
+    column_list = ["fullname", "username", "email", "birthdate", "avatar", "admin", "outcast"]
 
 
 def init_app(app):
