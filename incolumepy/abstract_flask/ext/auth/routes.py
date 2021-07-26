0a4745f75801d00c390fbd0edf5106c49dcf8c08
Britodfbr <britodfbr@gmail.com>
2021-07-25 22:38:29 -0300

    Formularios HTML e suas rotas

--- incolumepy/abstract_flask/ext/auth/routes.py
+++ incolumepy/abstract_flask/ext/auth/routes.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = '@britodfbr'
+__author__ = "@britodfbr"
 from flask import Blueprint, render_template, url_for, redirect, flash
 from flask_login import current_user, logout_user
 from incolumepy.abstract_flask.ext.dbase.models import User
 from incolumepy.abstract_flask.ext.dbase import db
 
-
-bp = Blueprint('auth', __name__, template_folder='templates')
+bp = Blueprint("auth", __name__, template_folder="templates")
 
 
 @bp.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('webui.home'))
-    form = None   # RegistrationForm()
+        return redirect(url_for("webui.home"))
+    form = None  # RegistrationForm()
     if form.validate_on_submit():
-        hashed_pw = bc.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        user = User(
+            fullname=form.fullname.data,
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+            birthdate=form.birthdate.data,
+        )
         db.session.add(user)
         db.session.commit()
         # flash(f'Conta "{form.email.data}" criada com sucesso!', "success")
