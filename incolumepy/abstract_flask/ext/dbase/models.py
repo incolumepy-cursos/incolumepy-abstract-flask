92050007c73dc3497b70b3858edc850f190bb181
Britodfbr <britodfbr@gmail.com>
2021-07-27 08:15:45 -0300

    Tratativa em resolver erro de recursão

--- incolumepy/abstract_flask/ext/dbase/models.py
+++ incolumepy/abstract_flask/ext/dbase/models.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = '@britodfbr'
+__author__ = "@britodfbr"
 import datetime as dt
 from flask_login import UserMixin
 from . import db, loginmanager, bc
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @loginmanager.user_loader
@@ -21,20 +22,22 @@ class User(db.Model, UserMixin):
     avatar = db.Column(db.String(20), nullable=False, default="user_color.svg")
     admin = db.Column(db.Boolean(), nullable=False, default=False)
     superadmin = db.Column(db.Boolean(), nullable=False, default=False)
-    outcast = db.Column(db.Boolean(), nullable=False, default=False)   # pária(outcast)/banido(banished)
+    outcast = db.Column(
+        db.Boolean(), nullable=False, default=False
+    )  # pária(outcast)/banido(banished)
     posts = db.relationship("Post", backref="author", lazy=True)
     comments = db.relationship("Comment", backref="comment", lazy=True)
 
     @property
     def password(self):
-        raise AttributeError('password not readable')
+        raise AttributeError("password not readable")
 
     @password.setter
     def password(self, pwd):
-        self.password = bc.generate_password_hash(pwd).decode('utf-8')
+        self.password = generate_password_hash(pwd)
 
     def check_password(self, pwd):
-        return bc.check_password_hash(self.password, pwd)
+        return check_password_hash(self.password, pwd)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.avatar}')"
@@ -58,9 +61,9 @@ class Post(db.Model):
     date_published = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     date_updated = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    media = db.Column(db.String(), default='')
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    posts = db.relationship('Comment', backref='post', lazy=True)
+    media = db.Column(db.String(), default="")
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    posts = db.relationship("Comment", backref="post", lazy=True)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.posted}')"
@@ -71,8 +74,8 @@ class Comment(db.Model):
     date_published = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     date_updated = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
 
     def __repr__(self):
         return f"Comment('{self.title}', '{self.posted}')"
@@ -85,86 +88,106 @@ class Category(db.Model):
     def __repr__(self):
         return f"Category('{self.name}')"
 
+
 users = [
-    {"id": 1,
-     "fullname": "Ricardo Brito do Nascimento",
-     "username": "britodfbr",
-     "email": "britodfbr+1@gmail.com",
-     "password": "123",
-     "avatar": "user_color.svg",
-     "admin": True,
-     "outcast": False},
-    {"id": 2,
-     "fullname": "Eliana Brito",
-     "username": "elianabrito",
-     "email": "britodfbr+2@gmail.com",
-     "password": "123",
-     "avatar": "user_color.svg",
-     "admin": False,
-     "outcast": False},
-    {"id": 3,
-     "fullname": "Ana Brito",
-     "username": "anabrito",
-     "email": "britodfbr+3@gmail.com",
-     "password": "123",
-     "avatar": "user_color.svg",
-     "admin": False,
-     "outcast": False},
-    {"id": 4,
-     "fullname": "Ada Brito",
-     "username": "adabrito",
-     "email": "britodfbr+4@gmail.com",
-     "password": "123",
-     "avatar": "user_color.svg",
-     "admin": False,
-     "outcast": False},
+    {
+        "id": 1,
+        "fullname": "Ricardo Brito do Nascimento",
+        "username": "britodfbr",
+        "email": "britodfbr+1@gmail.com",
+        "password": "123",
+        "avatar": "user_color.svg",
+        "admin": True,
+        "outcast": False,
+    },
+    {
+        "id": 2,
+        "fullname": "Eliana Brito",
+        "username": "elianabrito",
+        "email": "britodfbr+2@gmail.com",
+        "password": "123",
+        "avatar": "user_color.svg",
+        "admin": False,
+        "outcast": False,
+    },
+    {
+        "id": 3,
+        "fullname": "Ana Brito",
+        "username": "anabrito",
+        "email": "britodfbr+3@gmail.com",
+        "password": "123",
+        "avatar": "user_color.svg",
+        "admin": False,
+        "outcast": False,
+    },
+    {
+        "id": 4,
+        "fullname": "Ada Brito",
+        "username": "adabrito",
+        "email": "britodfbr+4@gmail.com",
+        "password": "123",
+        "avatar": "user_color.svg",
+        "admin": False,
+        "outcast": False,
+    },
 ]
 posts = [
-    {"id": "1",
-     "title": "Parabéns",
-     "date_published": "2021-7-5 14:15",
-     "date_updated": "2021-7-5 14:15",
-     "content": "Meu aniversário",
-     "media": "lamp-600.jpg",
-     "user_id": "4"},
-    {"id": "2",
-     "title": "Aniversário",
-     "date_published": "2021-7-5 14:15",
-     "date_updated": "2021-7-5 14:15",
-     "content": "Meu aniversário",
-     "media": "tulips-600.jpg",
-     "user_id": "3"},
-    {"id": "3",
-     "title": "Parabéns",
-     "date_published": "2021-7-5 14:15",
-     "date_updated": "2021-7-5 14:15",
-     "content": "Meu aniversário",
-     "media": "tulips-600.jpg",
-     "user_id": "2"},
-    {"id": "4",
-     "title": "Parabéns",
-     "date_published": "2021-7-5 14:15",
-     "date_updated": "2021-7-5 14:15",
-     "content": "Meu aniversário",
-     "media": "dew-600.jpg",
-     "user_id": "1"},
-    {"id": 5,
-     "title": "Just a Standard Format Post",
-     "content": "Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor"
-                " nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua"
-                " cillum in consequat consequat in culpa in anim.",
-     "date_published": "2021-7-22 15:38",
-     "date_updated": "2021-7-22 15:38",
-     "media": "woodcraft-600.jpg",
-     "user_id": "2"
-     },
-    {"id": 6,
-     "title": "Using Repetition and Patterns in Photography",
-     "content": "Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor"
-                " nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua"
-                " cillum in consequat consequat in culpa in anim.",
-     "date_published": "2020-10-22 15:38",
-     "date_updated": "2021-7-22 15:38",
-     "media": "woodcraft-600.jpg",
-     "user_id": 2},
+    {
+        "id": "1",
+        "title": "Parabéns",
+        "date_published": "2021-7-5 14:15",
+        "date_updated": "2021-7-5 14:15",
+        "content": "Meu aniversário",
+        "media": "lamp-600.jpg",
+        "user_id": "4",
+    },
+    {
+        "id": "2",
+        "title": "Aniversário",
+        "date_published": "2021-7-5 14:15",
+        "date_updated": "2021-7-5 14:15",
+        "content": "Meu aniversário",
+        "media": "tulips-600.jpg",
+        "user_id": "3",
+    },
+    {
+        "id": "3",
+        "title": "Parabéns",
+        "date_published": "2021-7-5 14:15",
+        "date_updated": "2021-7-5 14:15",
+        "content": "Meu aniversário",
+        "media": "tulips-600.jpg",
+        "user_id": "2",
+    },
+    {
+        "id": "4",
+        "title": "Parabéns",
+        "date_published": "2021-7-5 14:15",
+        "date_updated": "2021-7-5 14:15",
+        "content": "Meu aniversário",
+        "media": "dew-600.jpg",
+        "user_id": "1",
+    },
+    {
+        "id": 5,
+        "title": "Just a Standard Format Post",
+        "content": "Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor"
+        " nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua"
+        " cillum in consequat consequat in culpa in anim.",
+        "date_published": "2021-7-22 15:38",
+        "date_updated": "2021-7-22 15:38",
+        "media": "woodcraft-600.jpg",
+        "user_id": "2",
+    },
+    {
+        "id": 6,
+        "title": "Using Repetition and Patterns in Photography",
+        "content": "Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor"
+        " nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua"
+        " cillum in consequat consequat in culpa in anim.",
+        "date_published": "2020-10-22 15:38",
+        "date_updated": "2021-7-22 15:38",
+        "media": "woodcraft-600.jpg",
+        "user_id": 2,
+    },
 ]
